9456a4bb9182e2e27cae21412353d6c321a19694
rishideuno <rishideuno@gmail.com>
2025-07-15 14:54:41 +0530

    this for rever

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component} from '@angular/core';
+import {Component} from  "@angular/core"
+
+
 @Component({
-  selector:'pm-root',
-  template:`<h1>
-                {{myTitle}}
-                <div>Welcome By Rishi</div>
-            </h1>`
+  selector:"pm-root",
+  template:`<div>
+               <h1>{{pageTitle}}</h1>
+                <div><p>My First Component</p></div>
+            </div>`
 })
-export class AppComponent
-{
-  myTitle:String="First Angular Component";
+export class AppComponent{
+
+  pageTitle:string="Acme Product Management";
 }
